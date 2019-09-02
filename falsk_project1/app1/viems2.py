28b7ce9e7d504570197b0f6838354b0bc0c923b8
zhangyp <zhangyp@fapiao.com>
2019-09-02 22:22:06 +0800

    update study

--- falsk_project1/app1/viems2.py
+++ falsk_project1/app1/viems2.py
@@ -1,4 +1,9 @@
-from flask import Blueprint
+# -*- coding: utf-8 -*-
+
+
+from __future__ import unicode_literals  # 头部加上这句
+
+from flask import Blueprint, render_template
 
 blue = Blueprint('viems2', __name__)
 
@@ -6,4 +11,16 @@ blue = Blueprint('viems2', __name__)
 @blue.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@blue.route('/index/')
+def index():
+    return render_template("123.html")
+
+
+@blue.route('/mine/')
+def mine():
+    return render_template("mine.html")
+
+
 
--- falsk_project1/manage.py
+++ falsk_project1/manage.py
@@ -1,11 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
 from flask import Flask
+# from flask.session import Session
 from flask_script import Manager
+from flask_session import Session
+
 from app1.viems1 import blue as blue_v1
 from app1.viems2 import blue as blue_v2
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "46164ASD4F61A6D45SF16A2SD41F65ASD1F"
+app.config["SESSION_TYPE"] = "redis"
+Session(app=app)
 app.register_blueprint(blueprint=blue_v1)
 app.register_blueprint(blueprint=blue_v2)
 manage = Manager(app=app)
